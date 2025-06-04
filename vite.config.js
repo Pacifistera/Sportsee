9acc99aa243eab5d84fc1c2fd5733988c3de42e2
MaxD <max.deroubaix@gmail.com>
2025-06-04 19:47:08 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Sportsee/',
+  base: process.env.NODE_ENV === 'production' ? '/Sportsee/' : '/',
   plugins: [react()],
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
   },
 });
